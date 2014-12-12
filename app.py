a1af38ce24de712f597b4824dac18a312af34e71
Scott Gallagher-Starr <scott.gallagherstarr@gmail.com>
2014-12-12 15:25:44 -0800

    adding requirements; using gevent.

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
+from gevent import monkey; monkey.patch_all()
+
 from flask import Flask
 app = Flask(__name__)
 
+
+from gevent.wsgi import WSGIServer
+#from yourapplication import app
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+  return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  http_server = WSGIServer(('', 5000), app)
+  http_server.serve_forever()
+#    app.run(debug=True)
