30fcede1fc1a39713e14627df38fe23b43cdba12
Scott Gallagher-Starr <scott.gallagherstarr@gmail.com>
2014-12-15 11:30:39 -0800

    clearing out; working with tornado instead.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='Nori3',
-      version='0.0.1',
-      description='Nori - SUSHI manager',
-      author='Scott Gallagher-Starr',
-      author_email='scott.gallagherstarr@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-     )
